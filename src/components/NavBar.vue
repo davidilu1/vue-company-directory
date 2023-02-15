3d9dd69a168225d3c415b1023b734805170c8225
davidilu1 <davidilunga876@gmail.com>
2023-02-15 11:46:56 -0600

    addad contact button

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -12,6 +12,7 @@
       <div class="menu">
         <a href="#" class="menu-item">Departments</a>
         <a href="#" class="menu-item">Settings</a>
+        <a href="#" class="menu-item">Contact</a>
         <a href="#" class="menu-login">Logout</a>
       </div>
     </div>
