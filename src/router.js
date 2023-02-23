a756de3b6b67897207b4ac1dc86971b16ba184bd
davidilu1 <davidilunga876@gmail.com>
2023-02-23 10:05:03 -0600

    added firebase

--- src/router.js
+++ src/router.js
@@ -4,15 +4,14 @@ import { useAuth } from '@/composables/useAuth'
 const { isAuthenticated } = useAuth()
 
 import MainPage from '@/components/MainPage.vue'
-import LoginPage from '@/components/LoginPage.vue'
-
-import SettingsPage from '@/components/SettingsPage.vue'
+import LoginPage from '@/components/loginPage.vue'
+import SettingsPage from '@/components/settingsPage.vue'
 import NotFound from '@/components/NotFound.vue'
 
 const routes = [
     { path: '/', name: 'Home', component: MainPage },
-    { path: '/Login', name: 'Login', component: LoginPage },
-    { path: '/Settings', name: 'Settings', component: SettingsPage, meta: { requiresAuth: true } },
+    { path: '/login', name: 'Login', component: LoginPage },
+    { path: '/settings', name: 'Settings', component: SettingsPage, meta: { requiresAuth: true } },
     { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
 ]
 
